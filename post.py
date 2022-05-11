4d7671fefe8a7c40ed38225b7f171a1eaf27f6a0
murali <muralikatta212@gmail.com>
2022-05-11 12:32:55 +0530

    sdhgf

--- post.py
+++ post.py
@@ -8,12 +8,14 @@ def lambda_handler(event, context):
     movies=event['movies']
     director=event['director']
     actress=event['actress']
+    actor= event ['actor']
     language=event['language']
     try:
         table.put_item(Item={
             "movies":movies,
             "director" : director,
             "actress" : actress,
+            "actor" : actor,
             "language":language
             
         })
